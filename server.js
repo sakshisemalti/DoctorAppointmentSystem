48fa5f97f46fe1324381ef62dad3d4fa72b28c95
sakshisemalti <sakshisemalti58@gmail.com>
2023-03-06 20:02:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute");
+
+
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+
+
+const port = process.env.PORT || 5000;
+console.log(process.env.MONGO_URL);
+app.listen(port, () => console.log(`Listening in the port ${port}`));
+
+
